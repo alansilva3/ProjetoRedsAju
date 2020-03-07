620c7030d39f388e9786a5ac00e2cf2fc0fa1ab2
alansilva3 <jose.asilva@dcomp.ufs.br>
2020-03-07 13:51:03 -0300

    Testando REST

---
+++ src/main/java/com/dcomp/redsaju/resources/ProdutoResource.java
@@ -0,0 +1,15 @@
+package com.dcomp.redsaju.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value = "/produtos")
+public class ProdutoResource {
+
+	@GetMapping
+	public String listar() {
+		return "REST funcionando";
+	}
+}
--- src/test/java/com/dcomp/redsaju/ProjetoRedsAjuApplicationTests.java
+++ src/test/java/com/dcomp/redsaju/ProjetoRedsAjuApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.dcomp.redsaju;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ProjetoRedsAjuApplicationTests {
 
 	@Test
 	void contextLoads() {
