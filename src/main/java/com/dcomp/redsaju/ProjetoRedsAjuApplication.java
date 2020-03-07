b82042eb8b5bae92175586d21884524553af15de
alansilva3 <jose.asilva@dcomp.ufs.br>
2020-03-07 12:22:53 -0300

    first commit

---
+++ src/main/java/com/dcomp/redsaju/ProjetoRedsAjuApplication.java
@@ -0,0 +1,13 @@
+package com.dcomp.redsaju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRedsAjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRedsAjuApplication.class, args);
+	}
+
+}
