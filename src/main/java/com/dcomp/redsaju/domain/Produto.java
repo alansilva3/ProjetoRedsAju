d732fdfe8ab17f6a64b911391b114f32cb6c3c8e
alansilva3 <jose.asilva@dcomp.ufs.br>
2020-03-07 14:50:05 -0300

    Configurando o banco H2 para fase de testes

---
+++ src/main/java/com/dcomp/redsaju/domain/Produto.java
@@ -0,0 +1,110 @@
+package com.dcomp.redsaju.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
+	private String nome;
+	private String urlFoto;
+	private Double precoMercado;
+	private Double precoCusto;
+	private String descricao;
+	
+	public Produto() {
+	}
+
+	public Produto(Integer codigo,String nome, String urlFoto, Double precoMercado, Double precoCusto,
+			String descricao) {
+		super();
+		this.nome = nome;
+		this.codigo = codigo;
+		this.urlFoto = urlFoto;
+		this.precoMercado = precoMercado;
+		this.precoCusto = precoCusto;
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getUrlFoto() {
+		return urlFoto;
+	}
+
+	public void setUrlFoto(String urlFoto) {
+		this.urlFoto = urlFoto;
+	}
+
+	public Double getPrecoMercado() {
+		return precoMercado;
+	}
+
+	public void setPrecoMercado(Double precoMercado) {
+		this.precoMercado = precoMercado;
+	}
+
+	public Double getPrecoCusto() {
+		return precoCusto;
+	}
+
+	public void setPrecoCusto(Double precoCusto) {
+		this.precoCusto = precoCusto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+}
--- src/main/java/com/dcomp/redsaju/resources/ProdutoResource.java
+++ src/main/java/com/dcomp/redsaju/resources/ProdutoResource.java
@@ -1,15 +1,28 @@
 package com.dcomp.redsaju.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dcomp.redsaju.domain.Produto;
 
 @RestController
 @RequestMapping (value = "/produtos")
 public class ProdutoResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST funcionando";
+	public List<Produto> listar() {
+		
+		Produto prod1 = new Produto(1, "Camisa1","urlaqui",150.0, 80.0, "Camisa de um time random");
+		Produto prod2 = new Produto(2, "Camisa2","urlaqui",180.0, 100.0, "Camisa de um time random2");
+		
+		List<Produto> lista = new ArrayList<>();
+		lista.add(prod1);
+		lista.add(prod2);
+		
+		return lista;
 	}
 }
