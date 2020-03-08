9b4fd910b5db3241725a73ef9f2716f44a671273
alansilva3 <jose.asilva@dcomp.ufs.br>
2020-03-08 19:00:03 -0300

    testando redirecionamento para home page

---
+++ src/main/java/com/dcomp/redsaju/resources/HomeResource.java
@@ -0,0 +1,13 @@
+package com.dcomp.redsaju.resources;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeResource {
+
+	@RequestMapping (value = "/")
+	public String home() {
+		return "redirect:home.xhtml";
+	}
+}
